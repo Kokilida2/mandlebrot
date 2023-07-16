a72b3406681edaca3d3bfe0f4357eb4300140993
Nitzan Shaked <nitzans@nextsilicon.com>
2023-07-16 23:02:49 +0300

    added color

--- mandle.py
+++ mandle.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from matplotlib import pyplot as plt
 import math
 import numpy as np
+import colorsys
 
 #define size of the dots on the graph
 dotszie = 1
@@ -17,28 +20,32 @@ xrange = 2
 yrange = 1
 
 
-def draw_point(z: complex) -> None:
-    plt.plot(z.real,z.imag,marker='.', markersize=dotszie)
+def draw_point(z: complex, k: float) -> None:
+    r, g, b = colorsys.hls_to_rgb(k, 0.8, 1) if k is not None else (0, 0, 0)
+    plt.plot(
+        z.real,
+        z.imag,
+        marker='.',
+        markersize=dotszie,
+        c=(r, g, b)
+    )
 
-def is_mandlebrot(point: complex) -> bool:
+def is_mandlebrot(point: complex) -> int | None:
     z = 0
     c = point
     for i in range(accuracy):
-        try :
-            z = z**degree + c
-        except OverflowError:
-            # if we overflowed, it DEFINETLY converges
-            return False
-    # distance from origin. Does this diverge?
-    return (math.hypot(z.real, z.imag) < xrange*yrange)
+        z = z ** degree + c
+        if z.real ** 2 + z.imag ** 2 > 2 ** degree:
+            return i / accuracy
+    return None
 
 def calc_points():
     arr = np.linspace(-1*xrange,yrange,num=density)
     for a in arr:
         for b in arr:
-            c = complex(a,b)
-            if(is_mandlebrot(c)):
-                draw_point(c)
+            c = complex(a, b)
+            n_iter = is_mandlebrot(c)
+            draw_point(c, n_iter)
 
     # it's mandlebroting time!
     plt.grid()
